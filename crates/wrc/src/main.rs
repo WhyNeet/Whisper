6ea2ae17f810969f5b9af7076869e6e1c928a711
WhyNeet <arc.software.x@gmail.com>
2025-07-02 21:00:43 +0300

    feat(wrc): use parser

--- crates/wrc/src/main.rs
+++ crates/wrc/src/main.rs
@@ -1,12 +1,15 @@
+use lexer::stream::TokenStream;
+use parser::Parser;
 use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(path)?;
 
-    let mut token_stream = lexer::tokenize(&contents);
+    let token_stream = TokenStream::new(&contents);
+    let ast = Parser::new(token_stream).run();
 
-    dbg!(token_stream.next());
+    dbg!(ast);
 
     Ok(())
 }

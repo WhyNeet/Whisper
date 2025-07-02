21662ae2c00172ba13414ce76be21e3ea00a4058
WhyNeet <arc.software.x@gmail.com>
2025-07-02 22:05:19 +0300

    feat(wrc): use js-codegen

--- crates/wrc/src/main.rs
+++ crates/wrc/src/main.rs
@@ -1,15 +1,21 @@
+use js_codegen::JsCodegen;
 use lexer::stream::TokenStream;
 use parser::Parser;
 use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = env::args().nth(1).unwrap();
-    let contents = fs::read_to_string(path)?;
+    let contents = fs::read_to_string(&path)?;
 
     let token_stream = TokenStream::new(&contents);
     let ast = Parser::new(token_stream).run();
 
-    dbg!(ast);
+    let codegen = JsCodegen::new();
+    let output = codegen.generate_module(&ast);
+
+    let (outpath, _) = path.rsplit_once(".").unwrap();
+    let outpath = format!("{outpath}.js");
+    fs::write(outpath, output.as_bytes())?;
 
     Ok(())
 }

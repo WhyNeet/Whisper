dc6dc2a54256d0729eb2f57cc0a102aded158d88
WhyNeet <arc.software.x@gmail.com>
2025-07-03 13:29:50 +0300

    feat(ast): add function declaration

--- crates/ast/src/stmt.rs
+++ crates/ast/src/stmt.rs
@@ -1,6 +1,13 @@
 use crate::expr::Expression;
+use common::types::Type;
 
 #[derive(Debug, Clone)]
 pub enum Statement {
     Expression(Expression),
+    FunctionDeclaration {
+        name: String,
+        return_type: Type,
+        parameters: Vec<(String, Type)>,
+        body: Expression,
+    },
 }

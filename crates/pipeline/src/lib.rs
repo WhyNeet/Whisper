23d4f4ce884f3eeb80d4e5fc4b040b54f3d39de6
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-07-28 22:06:55 +0300

    refactor(pipeline): separate compilation steps into pipeline module

---
+++ crates/pipeline/src/lib.rs
@@ -0,0 +1,32 @@
+use checker::Checker;
+use js::{
+    codegen::Codegen as JsCodegen, transformer::TypedAstTransformer as JsTypedAstTransformer,
+};
+use lexer::stream::TokenStream;
+use parser::Parser;
+use tcast::module::Module;
+
+pub struct CompilationPipeline {}
+
+impl CompilationPipeline {
+    pub fn compile_module(contents: String) -> String {
+        let token_stream = TokenStream::new(&contents);
+        let module = Parser::new(token_stream).run();
+
+        let module = Checker::new().run(module);
+
+        let codegen = JsTypedAstTransformer::default();
+        let program = codegen.run(module);
+
+        let output = JsCodegen::default().run(program);
+
+        output
+    }
+
+    pub fn parse_module(contents: String) -> Module {
+        let token_stream = TokenStream::new(&contents);
+        let module = Parser::new(token_stream).run();
+
+        Checker::new().run(module)
+    }
+}
